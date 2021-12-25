5fe69f35feb2c2a7a231ecab9212392ca62dca9f
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-25 17:50:08 +0530

    .

--- Yuriko/modules/antiflood.py
+++ Yuriko/modules/antiflood.py
@@ -397,7 +397,7 @@ def __chat_settings__(chat_id, user_id):
         return "Not enforcing to flood control."
     return "Antiflood has been set to`{}`.".format(limit)
 
-__mod_name__ = "Fʟᴏᴏᴅ"
+__mod_name__ = "Flood 🏃‍♂️"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
