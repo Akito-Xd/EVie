253295bf397c25f3a0a8baaf93927d8e1510971b
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-28 20:09:24 +0530

    .

--- Yuriko/modules/tools.py
+++ Yuriko/modules/tools.py
@@ -1,61 +1,49 @@
-__mod_name__ = "Tᴏᴏʟꜱ"
+__mod_name__ = "Tools 〰️"
 
 __help__ = """
-*Yuriko Info bot*
+*EvieX Info bot*
 
-✗  /sg <reply> - `To check history name`
+/sg <reply> - `To check history name`
 
 *Date-time-Weather*
 
-✗  /time - `<country code> Gives information about a timezone.`
-
-✗  /weather - `<city> Get weather info in a particular place.`
-
-✗  /wttr - `<city> Advanced weather module, usage same as /weather`
-
-✗  /wttr - `moon Get the current status of moon`
+/time - `<country code> Gives information about a timezone.`
+/weather - `<city> Get weather info in a particular place.`
+/wttr - `<city> Advanced weather module, usage same as /weather`
+/wttr - `moon Get the current status of moon`
 
 *Converts*
 
-✗  /encrypt - `Encrypts The Given Text`
-
-✗  /decrypt - `Decrypts Previously Ecrypted Text`
-
-✗  /zip - `reply to a telegram file to compress it in .zip format`
-
-✗  /unzip - `reply to a telegram file to decompress it from the .zip format`
+/encrypt - `Encrypts The Given Text`
+/decrypt - `Decrypts Previously Ecrypted Text`
+/zip - `reply to a telegram file to compress it in .zip format`
+/unzip - `reply to a telegram file to decompress it from the .zip format`
 
 *Random API*
 
-✗  /ptl *or* /asupan - `get random video from Instagram timeline`
-
-✗  /chika - `get random video from chikakiku`
-
-✗  /wibu - `get random short anime video or photos`
-
-✗  /truth - `challenge`
-
-✗  /dare - `challenge`
+/ptl *or* /asupan - `get random video from Instagram timeline`
+/chika - `get random video from chikakiku`
+/wibu - `get random short anime video or photos`
+/truth - `challenge`
+/dare - `challenge`
 
 *Link To File:*
 
-✗  /up - `reply to a direct download link to upload it to telegram as files`
+/up - `reply to a direct download link to upload it to telegram as files`
  
  *File To Link:*
 
-✗  /transfersh - `reply to a telegram file to upload it on transfersh and get direct download link`
-
-✗  /tmpninja - `reply to a telegram file to upload it on tmpninja and get direct download link`
+/transfersh - `reply to a telegram file to upload it on transfersh and get direct download link`
+/tmpninja - `reply to a telegram file to upload it on tmpninja and get direct download link`
 
  *File Or Text To Telegraph:*
 
-✗  /tgm - `Get Telegraph Link Of Replied Media`
-
-✗  /tgt - `Get Telegraph Link of Replied Text`
+/tgm - `Get Telegraph Link Of Replied Media`
+/tgt - `Get Telegraph Link of Replied Text`
 
  *Tagger*
 
-✗  /tagall - `for tag everyone at least 100 member first.`
+/tagall - `for tag everyone at least 100 member first.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*Powered By: Isabella Fam 💕*
 """
