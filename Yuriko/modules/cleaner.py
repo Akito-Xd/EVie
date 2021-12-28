ae8f8b067afbe01311a9de16c297716154fb2fc4
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-28 12:25:00 +0530

    .

--- Yuriko/modules/cleaner.py
+++ Yuriko/modules/cleaner.py
@@ -217,19 +217,15 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
 __help__ = """
 *Blue text cleaner removed any made up commands that people send in your chat.*
 
-✗ /cleanblue - `<on/off/yes/no> clean commands after sending`
-
-✗ /ignoreblue - `<word> prevent auto cleaning of the command`
-
-✗ /unignoreblue - `<word> remove prevent auto cleaning of the command`
-
-✗ /listblue - `list currently whitelisted commands`
+/cleanblue - `<on/off/yes/no> clean commands after sending`
+/ignoreblue - `<word> prevent auto cleaning of the command`
+/unignoreblue - `<word> remove prevent auto cleaning of the command`
+/listblue - `list currently whitelisted commands`
 
  *Following are Disasters only commands, admins cannot use these:*
 
-✗ /gignoreblue - `<word> globally ignorea bluetext cleaning of saved word across DᴇCᴏᴅᴇ.`
-
-✗ /ungignoreblue - `<word> remove said command from global cleaning list`
+/gignoreblue - `<word> globally ignorea bluetext cleaning of saved word across DᴇCᴏᴅᴇ.`
+/ungignoreblue - `<word> remove said command from global cleaning list`
 
 *✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
 """
@@ -270,7 +266,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "Cʟᴇᴀɴᴇʀ"
+__mod_name__ = "Cleaner 🧹"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
