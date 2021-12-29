ee3b8ee79079280c8e5dbe884d8e3beda44a7e25
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-29 10:46:05 +0530

    .

--- Yuriko/modules/reporting.py
+++ Yuriko/modules/reporting.py
@@ -265,14 +265,13 @@ def buttons(update: Update, context: CallbackContext):
 
 
 __help__ = """
-✗ /report - `<reason> reply to a message to report it to admins.`
-
-✗ @admin - `reply to a message to report it to admins.`
+/report - `<reason> reply to a message to report it to admins.`
+@admin - `reply to a message to report it to admins.`
 
 *NOTE:* `Neither of these will get triggered if used by admins.`
 
 *Admins only:*
-✗ /reports - `<on/off> change report setting or view current status.`
+/reports - `<on/off> change report setting or view current status.`
 `If done in pm toggles your status.`
 `If in group toggles that groups status.`
 """
@@ -293,7 +292,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "Rᴇᴘᴏʀᴛ"
+__mod_name__ = "Report 📬"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
