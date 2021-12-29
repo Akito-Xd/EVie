e78e8b67a896a377eac21a2d51fae1cd5b890749
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-29 10:38:42 +0530

    .

--- Yuriko/modules/notes.py
+++ Yuriko/modules/notes.py
@@ -549,25 +549,22 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
 
-✗ /get - `<notename> get the note with this notename`
-
-✗ `<notename> same as /get`
-
-✗ /notes - `or /saved list all saved notes in this chat`
-
-✗ /number - `Will pull the note of that number in the list`
+/get - `<notename> get the note with this notename`
+`<notename> same as /get`
+/notes - `or /saved list all saved notes in this chat`
+/number - `Will pull the note of that number in the list`
 
 `If you would like to retrieve the contents of a note without any formatting, use` `/get <notename> noformat`. `This can`
 `be useful when updating a current note`
 
 *Admins only:*
 
-✗ /save -  `<notename> <notedata> saves notedata as a note with name notename`
+/save -  `<notename> <notedata> saves notedata as a note with name notename`
 
 *A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a*
 `buttonurl: ` *section, as such:* `[somelink](buttonurl:example.com)`. *Check* `/markdownhelp` *for more info*
 
-✗ /save - `<notename> save the replied message as a note with name notename`
+/save - `<notename> save the replied message as a note with name notename`
 
  `Separate diff replies by` `%%%` `to get random notes`
 
@@ -578,16 +575,15 @@ __help__ = """
  Reply 2
  %%%
  Reply 3`
-✗ /clear - `<notename> clear note with this name`
-
-✗ /removeallnotes - `removes all notes from the group`
+/clear - `<notename> clear note with this name`
+/removeallnotes - `removes all notes from the group`
 
  *Note:* `Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.`
 
-✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!
+*Powered By: Isabella Fam 💕*
 """
 
-__mod_name__ = "Nᴏᴛᴇꜱ"
+__mod_name__ = "Notes 📝"
 
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
