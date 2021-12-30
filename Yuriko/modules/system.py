e62d5053723ee5b549344005e50ae373649054bc
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-30 17:39:27 +0530

    .

--- Yuriko/modules/system.py
+++ Yuriko/modules/system.py
@@ -19,7 +19,7 @@ async def bot_sys_stats():
     disk = psutil.disk_usage("/").percent
     process = psutil.Process(os.getpid())
     stats = f"""
-root@EvieX:~$ EvieXRobot:
+root@im_bb:~$ EvieXBot:
 ------------------
 •⏳UPTIME: {formatter.get_readable_time((bot_uptime))}
 •📟BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
