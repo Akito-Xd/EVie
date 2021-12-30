44d5a05d6616a31c950c504694f0eaa2e216d321
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-31 00:13:25 +0530

    .

--- Yuriko/modules/notes.py
+++ Yuriko/modules/notes.py
@@ -549,38 +549,36 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
 
-/get - `<notename> get the note with this notename`
-`<notename> same as /get`
-/notes - `or /saved list all saved notes in this chat`
-/number - `Will pull the note of that number in the list`
+➮ /get - <notename> get the note with this notename
+➮ <notename> same as `/get`
+➮ /notes - or /saved list all saved notes in this chat
+➮ /number - Will pull the note of that number in the list
 
-`If you would like to retrieve the contents of a note without any formatting, use` `/get <notename> noformat`. `This can`
-`be useful when updating a current note`
+⍣ If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`.This canbe useful when updating a current note
 
-*Admins only:*
+Admins only:
 
-/save -  `<notename> <notedata> saves notedata as a note with name notename`
+➮ /save - <notename> <notedata> saves notedata as a note with name notename
 
-*A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a*
-`buttonurl: ` *section, as such:* `[somelink](buttonurl:example.com)`. *Check* `/markdownhelp` *for more info*
+⍣ A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a
+`buttonurl: ` *section, as such:* `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info
 
 /save - `<notename> save the replied message as a note with name notename`
 
- `Separate diff replies by` `%%%` `to get random notes`
+⍣ Separate diff replies by` `%%%` to get random notes
 
- *Example:*
- `/save notename
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
-/clear - `<notename> clear note with this name`
-/removeallnotes - `removes all notes from the group`
+*Example:*
+`/save notename
+Reply 1
+%%%
+Reply 2
+%%%
+Reply 3`
+➮ /clear - <notename> clear note with this name
+➮ /removeallnotes - removes all notes from the group
 
- *Note:* `Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.`
+*Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved
 
-*Powered By: Isabella Fam 💕*
 """
 
 __mod_name__ = "Notes 📝"
