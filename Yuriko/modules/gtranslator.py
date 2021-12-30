9612cab602a908f9417ebc5b16e2841d2e054151
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-30 21:39:34 +0530

    Update gtranslator.py

--- Yuriko/modules/gtranslator.py
+++ Yuriko/modules/gtranslator.py
@@ -16,21 +16,22 @@ from Yuriko.modules.disable import DisableAbleCommandHandler
 
 __help__ = """ 
 Use this module to translate stuff!
-*Commands:*
 
-/tl - `(or /tr) as a reply to a message, translates it to English.`
-/tl - `<lang>: translates to <lang>`
-/tl ja - `translates to Japanese.`
-/tl - `<source>//<dest> translates from <source> to <lang>.`
-/tl - `ja//en translates from Japanese to English.`
-/langs - `get a list of supported languages for translation.`
+Commands:
 
-`I can convert text to voice and voice to text..`
+➮ /tl - `(or /tr) as a reply to a message, translates it to English.`
+➮ /tl - `<lang>: translates to <lang>`
+➮ /tl ja - `translates to Japanese.`
+➮ /tl - `<source>//<dest> translates from <source> to <lang>.`
+➮ /tl - `ja//en translates from Japanese to English.`
+➮ /langs - `get a list of supported languages for translation.`
 
-/tts - `<lang code> Reply to any message to get text to speech output`
-/stt - `Type in reply to a voice message(support english only) to extract text from it.`
+I can convert text to voice and voice to text..
 
-*Language Codes*
+➮ /tts - `<lang code> Reply to any message to get text to speech output`
+➮ /stt - `Type in reply to a voice message(support english only) to extract text from it.`
+
+Language Codes:
 
 `af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
 et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
@@ -39,7 +40,6 @@ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 
-*Powered By: Isabella Fam 💕*
 """
 
 __mod_name__ = "Translator 🔡"
