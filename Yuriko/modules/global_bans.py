cf8ab97f8d65ba2f8d013c199e95babee8a51631
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-30 21:14:19 +0530

    .

--- Yuriko/modules/global_bans.py
+++ Yuriko/modules/global_bans.py
@@ -542,17 +542,17 @@ Admins only:
 
 ➮ /antispam - <on/off/yes/no> Will toggle our antispam tech or return your current settings
 
-➮ Anti-Spam used by bot devs to ban spammers across all groups. This helps protect!
+✇ Anti-Spam used by bot devs to ban spammers across all groups. This helps protect!
 you and your groups by removing spam flooders as quickly as possible
 *Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
 
-This also integrates `@Spamwatch` API to remove Spammers as much as possible from your chatroom!
+This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
 
 What is SpamWatch?
 
-➮ SpamWatch maintains a large constantly updated ban-list of spambots trolls bitcoin spammers and unsavoury characters
+✇ SpamWatch maintains a large constantly updated ban-list of spambots trolls bitcoin spammers and unsavoury characters
 Constantly help banning spammers off from your group automatically So you wont have to worry about spammers storming your group
-*Note:* Users can appeal spamwatch bans at @EvieXSupport
+*Note:* Users can appeal spamwatch bans at @SpamWatchSupport
 
 """
 
