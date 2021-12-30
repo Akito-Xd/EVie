762babeffac48a79c1c00cf095f4c9196226b5f9
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-30 21:01:26 +0530

    .

--- Yuriko/modules/fun.py
+++ Yuriko/modules/fun.py
@@ -313,41 +313,41 @@ def weebify(update: Update, context: CallbackContext):
 
 
 __help__ = """
-/runs - `reply a random string from an array of replies`
-/slap - `slap a user, or get slapped if not a reply`
-/shrug - `get shrug XD`
-/table - `get flip/unflip :v`
-/decide - `Randomly answers yes/no/maybe`
-/toss - `Tosses A coin`
-/bluetext - `check urself :V`
-/roll - `Roll a dice`
-/rlg - `Join ears,nose,mouth and create an emo ;-;`
-/shout - `<keyword> write anything you want to give loud shout`
-/weebify - `<text> returns a weebified text`
-/sanitize - `always use this before /pat or any contact`
-/pat - `pats a user, or get patted`
-/8ball - `predicts using 8ball method`
+➮ /runs - reply a random string from an array of replies
+➮ /slap - slap a user, or get slapped if not a reply
+➮ /shrug - get shrug XD
+➮ /table - get flip/unflip :v
+➮ /decide - Randomly answers yes/no/maybe
+➮ /toss - Tosses A coin
+➮ /bluetext - check urself :V
+➮ /roll - Roll a dice
+➮ /rlg - Join ears,nose,mouth and create an emo ;-;
+➮ /shout - <keyword> write anything you want to give loud shout
+➮ /weebify - <text> returns a weebified text
+➮ /sanitize - always use this before /pat or any contact
+➮ /pat - pats a user, or get patted
+➮ /8ball - predicts using 8ball method
 
-*Animation*
-✗ /love 
-✗ /hack 
-✗ /bombs 
+Animation:
 
-*Shippering*
-/couples - `get couples of today`
+➮ /love 
+➮ /hack 
+➮ /bombs 
 
-*Here Is The Help For The Styletext Module:*
+Shippering:
 
-/weebify - `<text>: weebify your text!`
-/bubble - `<text>: bubble your text!`
-/fbubble - `<text>: bubble-filled your text!`
-/square - `<text>: square your text!`
-/fsquare - `<text>: square-filled your text!`
-/blue - `<text>: bluify your text!`
-/latin - `<text>: latinify your text!`
-/lined - `<text>: lined your text!`
+➮ /couples - get couples of today
 
-*Powered By: Isabella Fam 💕*
+Here Is The Help For The Styletext Module:
+
+➮ /weebify - <text>: weebify your text!
+➮ /bubble - <text>: bubble your text!
+➮ /fbubble - <text>: bubble-filled your text!
+➮ /square - <text>: square your text!
+➮ /fsquare - <text>: square-filled your text!
+➮ /blue - <text>: bluify your text!
+➮ /latin - <text>: latinify your text!
+➮ /lined - <text>: lined your text!
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
