e17a3525cbec0b442d7a682e7f485ef2a84eab9a
Akito-Xd <91659054+Akito-Xd@users.noreply.github.com>
2021-12-30 21:52:40 +0530

    .

--- Yuriko/modules/locks.py
+++ Yuriko/modules/locks.py
@@ -566,31 +566,30 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-`Do stickers annoy you? or want to avoid people sharing links? or pictures?`
-`You're in the right place!`
-`The locks module allows you to lock away some common items in the`
-`telegram world; the bot will automatically delete them!`
+✇ Do stickers annoy you? or want to avoid people sharing links? or pictures?
+You're in the right place!
+✇ The locks module allows you to lock away some common items in the
+telegram world: the bot will automatically delete them!
 
-/locktypes - `Lists all possible locktypes`
+➮ /locktypes - Lists all possible locktypes
 
-*Admins only:*
+Admins only:
 
-/lock - `<type> Lock items of a certain type (not available in private)`
-/unlock - `<type> Unlock items of a certain type (not available in private)`
-/locks - `The current list of locks in this chat.`
+➮ /lock - `<type> Lock items of a certain type (not available in private)`
+➮ /unlock - `<type> Unlock items of a certain type (not available in private)`
+➮ /locks - `The current list of locks in this chat.`
 
-*Locks can be used to restrict a group's users.*
+Locks can be used to restrict a group's users
 
 eg:
-`Locking urls will auto-delete all messages with urls, locking stickers will restrict all`
-`non-admin users from sending stickers, etc.`
-`Locking bots will stop non-admins from adding bots to the chat.`
+✇ Locking urls will auto-delete all messages with urls, locking stickers will restrict all
+non-admin users from sending stickers, etc.
+✇ Locking bots will stop non-admins from adding bots to the chat
 
 *Note:*
-`Unlocking - `permission` *info* `will allow members (non-admins) to change the group information, such as the description or the group name`
-`Unlocking - `permission` *pin* `will allow members (non-admins) to pinned a message in a group`
+✇ Unlocking - permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
+✇ Unlocking - permission *pin* will allow members (non-admins) to pinned a message in a group
 
-*Powered By: Isabella Fam 💕*
 """
 
 __mod_name__ = "Locks 🔐"
